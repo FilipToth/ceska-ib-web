3229af519a7670dbc1ed056b9f39cb683df854d5
FilipToth <filip.toth@accelley.com>
2024-03-01 04:11:26 -0500

    Add static footer

--- src/pages/landing/landing.tsx
+++ src/pages/landing/landing.tsx
@@ -1,8 +1,10 @@
 import "assets/landing-1.css"
 import "assets/landing-2.css"
+import "assets/landing-footer.css"
 import "assets/landing-navbar.css"
 import flag from 'assets/un_flag.webp'
-import logo from 'assets/ib-world-school-logo-2-colour.png'
+import schoolLogo from 'assets/school-logo.png'
+import ibLogo from 'assets/ib-world-school-logo-2-colour.png'
 import EngagedButton from "components/engagedButton"
 import { Gradient } from "assets/gradient"
 import { useEffect } from "react"
@@ -20,7 +22,7 @@ const LandingPage = () => {
                 <div className="navbar-wrapper">
                     <div className="ib-logo-spacer" >
                         <div className="ib-logo-wrapper">
-                            <img className="ib-logo" src={logo}></img>
+                            <img className="ib-logo" src={ibLogo}></img>
                         </div>
                     </div>
                     <div className="navbar">
@@ -41,30 +43,76 @@ const LandingPage = () => {
                         <p className="gradient-about-text">The IB Diploma Programme molds leaders through rigorous academics, critical thinking, and social responsibility, preparing students for a connected world with confidence and empathy.</p>
                         <EngagedButton text={"About the IB"} onClick={() => {}} additionalStyle={ {'order': 2} }/>
                     </div>
-                    <p1>kqwuaw,ljdfn;ajkwnfck;janksjjnd</p1>
+                    <p>     wpiefj    Three.js Animation       </p>
                 </div>
             </div>
-{/*             <div className="landing-wrapper">
+            <div className="landing-wrapper">
                 <div className="landing-subpage-layout">
-                    <div className="landing-subpage-left-wrapper">
-                        <div className="landing-subpage-left-text-wrapper">
+                    <div>
+                        <div className="landing-subpage-left-wrapper">
                             <p className="landing-subpage-heading">An education like no other!</p>
                             <p className="landing-subpage-subheading">Lorem pahfujbaouifhiaebfo aeoufhapoeujfpou jhbwjgdvbaojefpi hbwa09di Lorem pahfujbaouifhiaebfo ]aeoufhapoeujfpou jhbwjgdvbaojefpi hbwa09di</p>
                             <EngagedButton text={"About the IB"} onClick={() => {}} additionalStyle={ {'order': 2} }/>
                         </div>
                     </div>
-                    <div className="landing-subpage-right-wrapper">
-                        <div className="center-cell-wrapper">
-                            <img className="ib-center-cell-image"></img>
-                            <p>The Student</p>
+                    <canvas width={600} height={600}></canvas>
+                </div>
+            </div>
+            <div className="landing-wrapper">
+                <div className="landing-subpage-layout">
+                    <div>
+                        <div className="landing-subpage-left-wrapper">
+                            <p className="landing-subpage-heading">Why Ceska?</p>
+                            <p className="landing-subpage-subheading">Lorem pahfujbaouifhiaebfo aeoufhapoeujfpou jhbwjgdvbaojefpi hbwa09di Lorem pahfujbaouifhiaebfo ]aeoufhapoeujfpou jhbwjgdvbaojefpi hbwa09di</p>
+                            <EngagedButton text={"About the IB"} onClick={() => {}} additionalStyle={ {'order': 2} }/>
                         </div>
-                        <div className="ib-cell-wrapper">
-                            <img className="ib-cell-wrapper"></img>
-                            <p>kl;</p>
+                    </div>
+                    <canvas width={600} height={600}></canvas>
+                </div>
+            </div>
+            <div className="footer-page-wrapper">
+                <div className="footer-wrapper">
+                    <div className="footer-content-wrapper">
+                        <div className="footer-content-group">
+                            <p className="footer-content-text">About Us</p>
+                            <p className="footer-content-text">About Us</p>
+                            <p className="footer-content-text">About Us</p>
+                            <p className="footer-content-text">About Us</p>
+                        </div>
+                        <div className="footer-content-group">
+                            <p className="footer-content-text">About Us</p>
+                            <p className="footer-content-text">About Us</p>
+                            <p className="footer-content-text">About Us</p>
+                            <p className="footer-content-text">About Us</p>
+                        </div>
+                        <div className="footer-content-group">
+                            <p className="footer-content-text">About Us</p>
+                            <p className="footer-content-text">About Us</p>
+                            <p className="footer-content-text">About Us</p>
+                            <p className="footer-content-text">About Us</p>
+                        </div>
+                        <div className="footer-content-group">
+                            <p className="footer-content-text">About Us</p>
+                            <p className="footer-content-text">About Us</p>
+                            <p className="footer-content-text">About Us</p>
+                            <p className="footer-content-text">About Us</p>
+                        </div>
+                    </div>
+                    <div className="footer-bottom-wrapper">
+                        <p className="school-name-text">© 2024 Sukromne Bilyngvalne Gymnazium Ceska 10</p>
+                        <div className="footer-bottom-right-wrapper">
+                            <div className="school-logo-wrapper">
+                                <img className="school-logo" src={schoolLogo}></img>
+                            </div>
+                            <div className="ib-logo-spacer" >
+                                <div className="ib-logo-wrapper">
+                                    <img className="ib-logo" src={ibLogo}></img>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div> */}
+            </div>
         </>
     )
 };
