010f3f4dce7482923504726b0554599492e4e978
FilipToth <filip.toth@accelley.com>
2024-03-03 18:18:53 -0500

    Add Links and Resources

--- src/components/navbar/navbarLinks.tsx
+++ src/components/navbar/navbarLinks.tsx
@@ -4,74 +4,46 @@ interface DropdownEntry {
 }
 
 const aboutUsItems: { [subCategory: string]: DropdownEntry[] } = {
+    "Policies": [
+        { text: "Academic Integrity", redirect: "other/academic-integrity.pdf" },
+        { text: "Assessment Policy", redirect: "other/assessment-policy.pdf" },
+        { text: "Inclusion Policy", redirect: "other/inclusion-policy.pdf" },
+        { text: "Language Policy", redirect: "other/language-policy.pdf" },
+        { text: "Newsletter", redirect: "other/newsletter.pdf" },
+        { text: "Org Chart", redirect: "other/org-chart.jpg" }
+    ],
     "Subjects": [
-        { text: "Math HL", redirect: "/src/assets/un_flag.webp" },
-        { text: "Slovek Ejch El", redirect: "/src/assets/un_flag.webp" },
-        { text: "Math HL", redirect: "/src/assets/un_flag.webp" },
-        { text: "Math HL", redirect: "/src/assets/un_flag.webp" },
-        { text: "Math HL", redirect: "/src/assets/un_flag.webp" },
-        { text: "Math HL", redirect: "/src/assets/un_flag.webp" },
-        { text: "Math HL", redirect: "/src/assets/un_flag.webp" },
-        { text: "Math HL", redirect: "/src/assets/un_flag.webp" },
-    ],
-    "Other Thimgamajig": [
-        { text: "Woof Woof", redirect: "/src/assets/un_flag.webp" },
-        { text: "Woof Woof", redirect: "/src/assets/un_flag.webp" },
-        { text: "Woof Woof", redirect: "/src/assets/un_flag.webp" },
-        { text: "Woof Woof", redirect: "/src/assets/un_flag.webp" },
-        { text: "Woof Woof", redirect: "/src/assets/un_flag.webp" },
-        { text: "Woof Woof", redirect: "/src/assets/un_flag.webp" },
-    ],
-    "Thiccc": [
-        { text: "HAHJKHJDN HL", redirect: "/src/assets/un_flag.webp" },
-        { text: "HAHJKHJDN HL", redirect: "/src/assets/un_flag.webp" },
-        { text: "HAHJKHJDN HL", redirect: "/src/assets/un_flag.webp" },
-        { text: "HAHJKHJDN HL", redirect: "/src/assets/un_flag.webp" },
+        { text: "Biology", redirect: "subjects/bio.pdf" },
+        { text: "Chemistry", redirect: "subjects/chem.pdf" },
+        { text: "Computer Science SL", redirect: "subjects/cs-sl.pdf" },
+        { text: "Computer Science HL", redirect: "subjects/cs-hl.pdf" },
+        { text: "Economics HL", redirect: "subjects/econ-hl.pdf" },
+        { text: "Economics SL", redirect: "subjects/econ-sl.pdf" },
+        { text: "Environmental Studies", redirect: "subjects/ess.pdf" },
+        { text: "Geography", redirect: "subjects/geo.pdf" },
+        { text: "Global Politics", redirect: "subjects/global-politics.pdf" },
+        { text: "History HL", redirect: "subjects/history-hl.pdf" },
+        { text: "History SL", redirect: "subjects/history-sl.pdf" },
+        { text: "Language A", redirect: "subjects/lang-a.pdf" },
+        { text: "Language B", redirect: "subjects/lang-b.pdf" },
+        { text: "Math AA", redirect: "subjects/math-aa.pdf" },
+        { text: "Math AI", redirect: "subjects/math-ai.pdf" },
+        { text: "Psychology", redirect: "subjects/psychology.pdf" }
     ]
 };
 
 const regularItems: { [category: string]: DropdownEntry[] } = {
     "Apply Now": [
-        { text: "Apply", redirect: "/src/assets/un_flag.webp" },
-        { text: "Apply", redirect: "/src/assets/un_flag.webp" },
-        { text: "Apply", redirect: "/src/assets/un_flag.webp" },
-        { text: "Apply", redirect: "/src/assets/un_flag.webp" },
-        { text: "Apply", redirect: "/src/assets/un_flag.webp" },
+        { text: "Admissions Criteria", redirect: "other/admissions-criteria.pdf" },
+        { text: "Application English", redirect: "other/application-english.pdf" },
+        { text: "Applicaton Slovak", redirect: "other/application-slovak.pdf" },
+        { text: "Apply", redirect: "other/.pdf" },
+        { text: "Apply", redirect: "other/.pdf" }
     ],
-    "For Students": [
-        { text: "Student Resource", redirect: "/src/assets/un_flag.webp" },
-        { text: "Student Resource", redirect: "/src/assets/un_flag.webp" },
-        { text: "Student Resource", redirect: "/src/assets/un_flag.webp" },
-        { text: "Student Resource", redirect: "/src/assets/un_flag.webp" },
-        { text: "Student Resource", redirect: "/src/assets/un_flag.webp" },
-        { text: "Student Resource", redirect: "/src/assets/un_flag.webp" },
-        { text: "Student Resource", redirect: "/src/assets/un_flag.webp" },
-        { text: "Student Resource", redirect: "/src/assets/un_flag.webp" },
-        { text: "Student Resource", redirect: "/src/assets/un_flag.webp" },
-        { text: "Student Resource", redirect: "/src/assets/un_flag.webp" },
-        { text: "Student Resource", redirect: "/src/assets/un_flag.webp" },
-        { text: "Student Resource", redirect: "/src/assets/un_flag.webp" },
-        { text: "Student Resource", redirect: "/src/assets/un_flag.webp" },
-        { text: "Student Resource", redirect: "/src/assets/un_flag.webp" },
-        { text: "Student Resource", redirect: "/src/assets/un_flag.webp" },
-        { text: "Student Resource", redirect: "/src/assets/un_flag.webp" },
-        { text: "Student Resource", redirect: "/src/assets/un_flag.webp" },
-    ],
-    "For Parents": [
-        { text: "Parent Thing", redirect: "/src/assets/un_flag.webp" },
-        { text: "Parent Thing", redirect: "/src/assets/un_flag.webp" },
-        { text: "Parent Thing", redirect: "/src/assets/un_flag.webp" },
-        { text: "Parent Thing", redirect: "/src/assets/un_flag.webp" },
-        { text: "Parent Thing", redirect: "/src/assets/un_flag.webp" },
-        { text: "Parent Thing", redirect: "/src/assets/un_flag.webp" },
-    ],
-    "Events": [
-        { text: "A BORING EVENT", redirect: "/src/assets/un_flag.webp" },
-        { text: "A BORING EVENT", redirect: "/src/assets/un_flag.webp" },
-        { text: "A BORING EVENT", redirect: "/src/assets/un_flag.webp" },
-        { text: "A BORING EVENT", redirect: "/src/assets/un_flag.webp" },
-        { text: "A BORING EVENT", redirect: "/src/assets/un_flag.webp" },
-        { text: "A BORING EVENT", redirect: "/src/assets/un_flag.webp" },
+    "IB Core": [
+        { text: "CAS Handbook", redirect: "other/cas-handbook.pdf" },
+        { text: "TOK", redirect: "other/tok.pdf" },
+        { text: "EE", redirect: "other/ee.pdf" }
     ]
 };
 
